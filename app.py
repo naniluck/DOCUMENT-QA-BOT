808fdf46f172b8dcedf07b5088b36a363a137cab
Maradana Hemanth <maradananani19@gmail.com>
2026-06-20 16:36:34 +0530

    Add files via upload
    Initial commit of Document QA Bot

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import chromadb
+from chromadb.utils import embedding_functions
+
+st.title("Document QA Bot")
+
+# DB connection logic
+embedding_fn = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
+client = chromadb.PersistentClient(path="./db")
+collection = client.get_collection(name="document_knowledge_base", embedding_function=embedding_fn)
+
+query = st.text_input("Meeru adhagalanukune prashna ikkada type cheyandi:")
+
+if st.button("Submit"):
+    if query:
+        results = collection.query(query_texts=[query], n_results=2)
+        if results['documents']:
+            for doc in results['documents'][0]:
+                st.write(f"-> {doc}")
+        else:
+            st.write("Samaacharam dorakaledhu.")
